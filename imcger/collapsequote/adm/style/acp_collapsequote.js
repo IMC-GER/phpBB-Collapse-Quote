8927c63b6955b6e20973058ac53c1a79edd38fd3
Thorsten <88725484+IMC-GER@users.noreply.github.com>
2024-04-18 03:07:30 +0200

    v1.3.3-b2
    - LukeWCSphpBBConfirmBox 1.4.3

--- imcger/collapsequote/adm/style/acp_collapsequote.js
+++ imcger/collapsequote/adm/style/acp_collapsequote.js
@@ -9,74 +9,75 @@
 
 var imcgerCollapseQuote = {};
 
-(function () {	// IIFE start
+(function ($) {	// IIFE start
 
 'use strict';
 
 class LukeWCSphpBBConfirmBox {
 /*
-* phpBB ConfirmBox class for checkboxes - v1.1.0
+* phpBB ConfirmBox class for checkboxes and yes/no radio buttons - v1.4.3
 * @copyright (c) 2023, LukeWCS, https://www.wcsaga.org
 * @license GNU General Public License, version 2 (GPL-2.0-only)
 */
-	constructor(submitSelector) {
-		this.$submitObject = $(submitSelector);
-		var _this = this;
+	constructor(submitSelector, animDuration = 0) {
+		let _this = this;
+		this.$submitObject	= $(submitSelector);
+		this.$formObject	= this.$submitObject.parents('form');
+		this.animDuration	= animDuration;
 
-		$('div[id$="_confirmbox"]').each(function () {
-			var elementName = $(this)[0].id.replace('_confirmbox', '')
+		this.$formObject.find('div[id$="_confirmbox"]').each(function () {
+			const elementName = this.id.replace('_confirmbox', '');
 
-			$('input[name="' + elementName + '"]')				.on('change'	, _this.Show);
-			$('input[name^="' + elementName + '_confirm_"]')	.on('click'		, _this.Button);
+			$('input[name="' + elementName + '"]')				.on('change'	, _this.#Show);
+			$('input[name^="' + elementName + '_confirm_"]')	.on('click'		, _this.#Button);
 		});
-		this.$submitObject.parents('form')						.on('reset'		, this.HideAll);
+		this.$formObject										.on('reset'		, _this.HideAll);
 	}
 
-	Show = (e) => {
-		var elementDefault = $('div[id="' + e.target.name + '_confirmbox"]').attr('data-default') == 1;
-		var $elementObject = $('input[name="' + e.target.name + '"]');
+	#Show = (e) => {
+		const $elementObject	= $('input[name="' + e.target.name + '"]');
+		const $confirmBoxObject	= $('div[id="' + e.target.name + '_confirmbox"]');
 
-		if ($elementObject.prop('checked') != elementDefault) {
-			$elementObject									.prop('disabled', true)
-			$elementObject									.addClass('confirmbox_active');
-			$('div[id="' + e.target.name + '_confirmbox"]')	.toggle('scale');
-			this.$submitObject								.prop('disabled', true);
+		if ($elementObject.prop('checked') != $confirmBoxObject.attr('data-default')) {
+			this.#changeBoxState($elementObject, $confirmBoxObject, true);
 		}
 	}
 
-	Button = (e) => {
-		var elementName = e.target.name.replace(/_confirm_.*/, '');
-		var elementDefault = $('div[id="' + elementName + '_confirmbox"]').attr('data-default') == 1;
-		var $elementObject = $('input[name="' + elementName + '"]');
+	#Button = (e) => {
+		const elementName		= e.target.name.replace(/_confirm_.*/, '');
+		const $elementObject	= $('input[name="' + elementName + '"]');
+		const $confirmBoxObject	= $('div[id="' + elementName + '_confirmbox"]');
+		const elementType		= $elementObject.attr('type');
 
 		if (e.target.name.endsWith('_confirm_no')) {
-			$elementObject.prop('checked', elementDefault);
+			if (elementType == 'checkbox') {
+				$elementObject.prop('checked', $confirmBoxObject.attr('data-default'));
+			} else if (elementType == 'radio') {
+				$elementObject.filter('[value="' + ($confirmBoxObject.attr('data-default') ? '1' : '0') + '"]').prop('checked', true);
+			}
 		}
-
-		$elementObject									.prop('disabled', false);
-		$elementObject									.removeClass('confirmbox_active');
-		$('div[id="' + elementName + '_confirmbox"]')	.toggle('scale');
-		this.$submitObject								.prop('disabled', $('input[class*="confirmbox_active"]').length);
+		this.#changeBoxState($elementObject, $confirmBoxObject, null);
 	}
 
 	HideAll = () => {
-		var $elementObject = $('input[class*="confirmbox_active"]');
+		const $elementObject	= this.$formObject.find('input.confirmbox_active');
+		const $confirmBoxObject	= this.$formObject.find('div[id$="_confirmbox"]');
 
-		$elementObject				.prop('disabled', false);
-		$elementObject				.removeClass('confirmbox_active');
-		$('div[id$="_confirmbox"]').each(function() {
-			if ($(this).is(":visible")) {
-				$(this).toggle("scale")
-			}
-		});
-		this.$submitObject			.prop('disabled', false);
+		this.#changeBoxState($elementObject, $confirmBoxObject, false);
+	}
+
+	#changeBoxState = ($elementObject, $confirmBoxObject, showBox) => {
+		$elementObject		.prop('disabled', !!showBox);
+		$elementObject		.toggleClass('confirmbox_active', !!showBox);
+		$confirmBoxObject	[showBox ? 'show' : 'hide'](this.animDuration);
+		this.$submitObject	.prop('disabled', showBox ?? this.$formObject.find('input.confirmbox_active').length);
 	}
 }
 
 // Register events
 
 $(window).ready(function() {
-	imcgerCollapseQuote.ConfirmBox = new LukeWCSphpBBConfirmBox('input[name="submit"]');
+	imcgerCollapseQuote.ConfirmBox = new LukeWCSphpBBConfirmBox('input[name="submit"]', 300);
 });
 
-})();	// IIFE end
+})(jQuery);	// IIFE end
